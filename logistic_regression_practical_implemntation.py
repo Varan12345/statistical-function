84f44323d3dabf2bbbdb0b3a3899a2f7ffa5bd49
Varan singh <varansingh2409@gmail.com>
2026-03-31 23:37:42 +0530

    added confusin metrices

--- logistic_regression_practical_implemntation.py
+++ logistic_regression_practical_implemntation.py
@@ -14,19 +14,19 @@ print(df.isnull().sum())
 df['species']=df['species'].str.strip().str.lower()
 df['species']=df['species'].str.replace('iris-','')
 print(df['species'].unique())
-print(df[df['species']!='setosa'])
+print(df[df['species']!='setosa']) 
 print(df.head())
-df['species']=df['species'].map({'versicolor':0,'virginica':1})
+# df['species']=df['species'].map({'versicolor':0,'virginica':1})
 print(df.head())
 ## split this datset into indepndent amd dependent features
 
 from sklearn.preprocessing import LabelEncoder
-
+df=df[df['species']!='setosa']
+df['species']=df['species'].map({'versicolor':0,'virginica':1})
 # X and y
 X = df.drop('species', axis=1)
 
-le = LabelEncoder()
-y = le.fit_transform(df['species'])
+y=df['species']
 
 # split
 from sklearn.model_selection import train_test_split
@@ -48,14 +48,81 @@ grid = GridSearchCV(classifier, param_grid=parameter, scoring='accuracy', cv=5)
 grid.fit(X_train, y_train)
 
 print(grid.best_params_)
+
 print(grid.best_score_)
 
 y_pred =grid.predict(X_test)
 
 from sklearn.metrics import accuracy_score,classification_report
 
-score=accuracy_score(y_pred,y_test)
+score=accuracy_score(y_test,y_pred)
 print(score)
+
+
+
 
 sns.pairplot(df,hue='species')
 plt.show()
+# confusion materices  
+# actual | predited
+# 1|1        true postive(tp)
+# 0|0        true negative(tn)
+# 0|1        false postive(fp)
+# 1|0        false negative(fn)
+
+# accuracy=is the ration of corretly predictd observation to the total observation
+#  accuracy =tp+tn/tp+tn+fp+fn
+# prcesion= is the ration of correct observation to the total prediction post+ observation
+#  precision=tp/tp+fp
+# recall= is the ration of correctly predicted observation to all postive observation
+# recall =tp/tp+fn 
+
+# f beta score (1+b^2) precison*recall/b^2[precison+recall]
+# fp&fn both are imp. and when we both have to perform precison and recll(b=1)//hatmonic mean
+#  fp>fn fp is more imp than fn (b=0.5)
+# fn>>fp (b=2)
+#  f1 score is the harmonic mean of precison recall
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+
+# prediction already hai
+# y_test = actual
+# y_pred = model prediction
+
+# Accuracy
+acc = accuracy_score(y_test, y_pred)
+print("Accuracy:", acc)
+
+# Precision
+prec = precision_score(y_test, y_pred)
+print("Precision:", prec)
+
+# Recall
+rec = recall_score(y_test, y_pred)
+print("Recall:", rec)
+
+# F1 Score
+f1 = f1_score(y_test, y_pred)
+print("F1 Score:", f1)
+
+# Confusion Matrix
+cm = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:\n", cm)
+
+for actual, pred in zip(y_test,y_pred):print(actual, pred )
+
+tp=tn=fp=fn=0
+for actual, pred in zip(y_test,y_pred): 
+    if actual==1 and pred==1:
+        tp+=1
+    elif actual==0 and pred==0:
+     tn+=1
+    elif actual==0 and pred==1:
+      fp+=1
+    elif actual==1 and pred==0:
+      fn+=1
+
+      print("TP:",tp)
+      print("TN:",tn)
+      print("FP:", fp)
+      print("FN:", fn)
--- statistical_function.py
+++ statistical_function.py
@@ -7,7 +7,7 @@ import seaborn as sns
 # print (np.sort(ar))
 
 data=[2,3,3,4,4,4,5,5,5,5,6,6,6,6,6,7,7,7,7,7,7,8,8,8,8,8,9,9,9,9,10,10,10,11,11,12]
-df=pd.DataFrame({"x":data})
+df=pd.DataFrame({"x":data}) 
 print(df["x"].skew())
 print(df["x"].mean(),df["x"].median(),df["x"].mode())
 
